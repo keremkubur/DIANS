556a4e1d4adde7b66976a9b48dffd6aaffd68c78
Sreten <sretenjevtic01@gmail.com>
2021-11-12 20:28:16 +0100

    Commit Test

---
+++ src/main/java/dians/domasna/dians_lab_test/DiansLabTestApplication.java
@@ -0,0 +1,13 @@
+package dians.domasna.dians_lab_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiansLabTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiansLabTestApplication.class, args);
+    }
+
+}
