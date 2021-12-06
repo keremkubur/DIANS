ac313ee79883073685cd7d13932b2d5e01090156
keremkubur <79008501+keremkubur@users.noreply.github.com>
2021-12-06 22:30:59 +0100

    Delete angularclcient directory

--- angularclcient/src/app/components/map/model/parking-spot.spec.ts
+++
@@ -1,7 +0,0 @@
-import {ParkingSpot} from './parking-spot';
-
-describe('ParkingSpot', () => {
-    it('should create an instance', () => {
-        expect(new ParkingSpot()).toBeTruthy();
-    });
-});
