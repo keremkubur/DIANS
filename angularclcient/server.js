c180b5409d3bd4845fc5d19ee434b8971dd34350
Sreten <sretenjevtic01@gmail.com>
2021-12-25 16:03:17 +0100

    Angular Restructuring v2

--- angularclcient/server.js
+++ angularclcient/server.js
@@ -6,13 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
+app.use(express.static('./dist/angularclcient'));
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(__dirname+'/dist/angularclcient');
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
