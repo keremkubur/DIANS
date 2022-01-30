fda6199ceecb1ff5037be8534261e395ad65a662
Sreten <sretenjevtic01@gmail.com>
2022-01-30 17:59:18 +0100

    Refactoring older methods

--- angularclcient/src/server.js
+++ angularclcient/src/server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname,'/dist/angularclcient/')));
+app.use(express.static(path.join(__dirname,'../dist/angularclcient/')));
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/angularclcient/'));
+  res.sendFile(path.join(__dirname,'../dist/angularclcient/'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
